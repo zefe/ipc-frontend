2be882940ac846e99a5f79a26fd8021595ec7e2d
zefetorres <zefetorres@icloud.com>
2021-06-18 13:52:36 -0500

    add documentation errors

--- src/utils/errorMessagesHandler.js
+++ src/utils/errorMessagesHandler.js
@@ -1,3 +1,17 @@
+/*
+    Funcion maneja los errores del formulario de registro, recibe 2 parametros el primero es @errors
+    es el objeto que contiene las validaciones que no pasaron en el backend
+    ejem:
+    email: {
+        location: "body",
+        msg: "Email is required",
+        param: "Email"
+    }
+    El segundo para metro @e es el objeto que retorna el Request, headers,status, data etc.
+    Se utiliza para verificar el tipo de error y asi retornar solo los errores que nos envie la
+    solicitud.
+
+*/
 export const errorMessages = (errors, e) => {
     
     let nameError;
